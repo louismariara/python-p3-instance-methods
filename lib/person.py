2d7adbc7c00e8a74d3536ba91ca7ba1168bca974
Louis Githogori Mariara <louismariara@gmail.com>
2024-12-02 22:13:54 +0300

    instance correct

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,13 @@
 class Person:
     # Class body goes here
 
+
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+fido=Person()
+fido.talk()
+fido.walk()
+
     #Instance method definition
-    pass
